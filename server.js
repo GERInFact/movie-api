a8a59b039f446dcb3459e0ec613f632ae5a06dd3
M. Hille <marciausgoslar@gmail.com>
2019-08-17 20:36:26 +0200

    Added error handling to server.js http request

--- server.js
+++ server.js
@@ -8,18 +8,28 @@ http
   .createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
 
-    let filePath = "";
+    const pathName = parsedUrl.pathname;
+    if (!pathName) res.statusCode(500).json({ message: "No pathname." });
 
-    if (parsedUrl.pathname.includes("documentation"))
+    let filePath = "";
+    if (pathName.includes("documentation"))
       filePath = __dirname + "/documentation.html";
     else filePath = "index.html";
 
-    fs.readFile(filePath, (err, data) => {
-      if (err) throw err;
+    try {
+      fs.readFile(filePath, (err, data) => {
+        if (err) throw err;
 
-      res.writeHead(200, {'Content-type' : 'text/html'});
-      res.write(data);
-      res.end();
-    });
+        res.writeHead(200, { "Content-type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    } catch (err) {
+      console.log(err);
+      fs.appendFile(
+        "debug_log.txt",
+        "Error: " + err + " Timestamp: " + new Date() + "\n"
+      );
+    }
   })
   .listen(8080);
