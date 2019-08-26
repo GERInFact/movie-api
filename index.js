c7aa27cdd307a1ce0628033376050b435faa5ee6
M. Hille <marciausgoslar@gmail.com>
2019-08-26 19:19:36 +0200

    Added error logging for express

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+// essential web server and logging modules
 const express = require("express"),
   morgan = require("morgan"),
-  bodyParser = require("body-parser"),
   Movie = require("./Movie");
+
 const app = express();
 
+// middleware functions
 app.use(morgan("common"));
 app.use(express.static('public'));
 app.use((err, req, res, next) => {
-  // error-handling logic
+  console.log(err);
+  res.status(500).send("Something went wrong");
+  next();
 });
 
+// send welcome text on page visit
 app.get("/", (req, res) => {
   res.send("Welcome to myFlix! Your customized movie platform.");
 });
+
+// send top 10 movie list, if requested
 app.get("/movies", (req, res) => {
   res.json([
     new Movie(
@@ -98,4 +105,5 @@ app.get("/movies", (req, res) => {
   ]);
 });
 
+// listen on port 8080 
 app.listen(8080, () => console.log("Server is listening on port 8080"));
