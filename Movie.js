e24f5547f749c32c87469b507cf293cd38e94390
M. Hille <marciausgoslar@gmail.com>
2019-08-26 19:15:41 +0200

    Added movie class and routing to get top 10 movies

---
+++ Movie.js
@@ -0,0 +1,12 @@
+class Movie {
+    constructor(title, author, releaseDate, views, rating, description) {
+        this.title = title;
+        this.author = author;
+        this.releaseDate = releaseDate;
+        this.views = views;
+        this.rating = rating;
+        this.description = description;
+    }
+}
+
+module.exports = Movie;
--- index.js
+++ index.js
@@ -1,18 +1,101 @@
-const express = require("express"), morgan = require('morgan'), bodyParser = require('body-parser');
+const express = require("express"),
+  morgan = require("morgan"),
+  bodyParser = require("body-parser"),
+  Movie = require("./Movie");
 const app = express();
 
-app.use(morgan('common'));
-app.use(bodyParser.json());
-app.use((err,req,res,next) => {
+app.use(morgan("common"));
+app.use(express.static('public'));
+app.use((err, req, res, next) => {
   // error-handling logic
-})
-
-app.get("/", (req, res) => {
-  res.send("Hello Node!");
 });
 
-app.get("/documentation", (req, res) => {
-  res.sendFile("documentation.html", { root: __dirname });
+app.get("/", (req, res) => {
+  res.send("Welcome to myFlix! Your customized movie platform.");
+});
+app.get("/movies", (req, res) => {
+  res.json([
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    )
+  ]);
 });
 
 app.listen(8080, () => console.log("Server is listening on port 8080"));
