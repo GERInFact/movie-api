1d30ba50ad580830cff602b40345f446a47a5bdc
M. Hille <marciausgoslar@gmail.com>
2019-08-30 08:20:28 +0200

    Movied dummy movie data to own module

---
+++ Movies.js
@@ -0,0 +1,82 @@
+module.exports = [
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    ),
+    new Movie(
+      "Lord of the Rings",
+      "J.R.R. Tolkien",
+      new Date(),
+      Math.random() * 1000,
+      Math.random() * 5,
+      "An epic movie."
+    )];
+  
