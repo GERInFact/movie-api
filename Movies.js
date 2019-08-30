53e0e9c3fd878a0c286c2ed7ca4cca2fdc44e5c5
M. Hille <marciausgoslar@gmail.com>
2019-08-30 08:22:25 +0200

    Added lord of the rings url to movie list

--- Movies.js
+++ Movies.js
@@ -1,82 +1,92 @@
 module.exports = [
-    new Movie(
-      "Lord of the Rings",
-      "J.R.R. Tolkien",
-      new Date(),
-      Math.random() * 1000,
-      Math.random() * 5,
-      "An epic movie."
-    ),
-    new Movie(
-      "Lord of the Rings",
-      "J.R.R. Tolkien",
-      new Date(),
-      Math.random() * 1000,
-      Math.random() * 5,
-      "An epic movie."
-    ),
-    new Movie(
-      "Lord of the Rings",
-      "J.R.R. Tolkien",
-      new Date(),
-      Math.random() * 1000,
-      Math.random() * 5,
-      "An epic movie."
-    ),
-    new Movie(
-      "Lord of the Rings",
-      "J.R.R. Tolkien",
-      new Date(),
-      Math.random() * 1000,
-      Math.random() * 5,
-      "An epic movie."
-    ),
-    new Movie(
-      "Lord of the Rings",
-      "J.R.R. Tolkien",
-      new Date(),
-      Math.random() * 1000,
-      Math.random() * 5,
-      "An epic movie."
-    ),
-    new Movie(
-      "Lord of the Rings",
-      "J.R.R. Tolkien",
-      new Date(),
-      Math.random() * 1000,
-      Math.random() * 5,
-      "An epic movie."
-    ),
-    new Movie(
-      "Lord of the Rings",
-      "J.R.R. Tolkien",
-      new Date(),
-      Math.random() * 1000,
-      Math.random() * 5,
-      "An epic movie."
-    ),
-    new Movie(
-      "Lord of the Rings",
-      "J.R.R. Tolkien",
-      new Date(),
-      Math.random() * 1000,
-      Math.random() * 5,
-      "An epic movie."
-    ),
-    new Movie(
-      "Lord of the Rings",
-      "J.R.R. Tolkien",
-      new Date(),
-      Math.random() * 1000,
-      Math.random() * 5,
-      "An epic movie."
-    ),
-    new Movie(
-      "Lord of the Rings",
-      "J.R.R. Tolkien",
-      new Date(),
-      Math.random() * 1000,
-      Math.random() * 5,
-      "An epic movie."
-    )];
-  
+  new Movie(
+    "Lord of the Rings",
+    "J.R.R. Tolkien",
+    new Date(),
+    Math.random() * 1000,
+    Math.random() * 5,
+    "An epic movie.",
+    "https://itunes.apple.com/us/movie-collection/the-lord-of-the-rings-trilogy/id365647689"
+  ),
+  new Movie(
+    "Lord of the Rings",
+    "J.R.R. Tolkien",
+    new Date(),
+    Math.random() * 1000,
+    Math.random() * 5,
+    "An epic movie.",
+    "https://itunes.apple.com/us/movie-collection/the-lord-of-the-rings-trilogy/id365647689"
+  ),
+  new Movie(
+    "Lord of the Rings",
+    "J.R.R. Tolkien",
+    new Date(),
+    Math.random() * 1000,
+    Math.random() * 5,
+    "An epic movie.",
+    "https://itunes.apple.com/us/movie-collection/the-lord-of-the-rings-trilogy/id365647689"
+  ),
+  new Movie(
+    "Lord of the Rings",
+    "J.R.R. Tolkien",
+    new Date(),
+    Math.random() * 1000,
+    Math.random() * 5,
+    "An epic movie.",
+    "https://itunes.apple.com/us/movie-collection/the-lord-of-the-rings-trilogy/id365647689"
+  ),
+  new Movie(
+    "Lord of the Rings",
+    "J.R.R. Tolkien",
+    new Date(),
+    Math.random() * 1000,
+    Math.random() * 5,
+    "An epic movie.",
+    "https://itunes.apple.com/us/movie-collection/the-lord-of-the-rings-trilogy/id365647689"
+  ),
+  new Movie(
+    "Lord of the Rings",
+    "J.R.R. Tolkien",
+    new Date(),
+    Math.random() * 1000,
+    Math.random() * 5,
+    "An epic movie.",
+    "https://itunes.apple.com/us/movie-collection/the-lord-of-the-rings-trilogy/id365647689"
+  ),
+  new Movie(
+    "Lord of the Rings",
+    "J.R.R. Tolkien",
+    new Date(),
+    Math.random() * 1000,
+    Math.random() * 5,
+    "An epic movie.",
+    "https://itunes.apple.com/us/movie-collection/the-lord-of-the-rings-trilogy/id365647689"
+  ),
+  new Movie(
+    "Lord of the Rings",
+    "J.R.R. Tolkien",
+    new Date(),
+    Math.random() * 1000,
+    Math.random() * 5,
+    "An epic movie.",
+    "https://itunes.apple.com/us/movie-collection/the-lord-of-the-rings-trilogy/id365647689"
+  ),
+  new Movie(
+    "Lord of the Rings",
+    "J.R.R. Tolkien",
+    new Date(),
+    Math.random() * 1000,
+    Math.random() * 5,
+    "An epic movie.",
+    "https://itunes.apple.com/us/movie-collection/the-lord-of-the-rings-trilogy/id365647689"
+  ),
+  new Movie(
+    "Lord of the Rings",
+    "J.R.R. Tolkien",
+    new Date(),
+    Math.random() * 1000,
+    Math.random() * 5,
+    "An epic movie.",
+    "https://itunes.apple.com/us/movie-collection/the-lord-of-the-rings-trilogy/id365647689"
+  )
+];
