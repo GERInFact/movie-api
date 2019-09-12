ba5c292b6a9acc73a10146c09431bcedcbf688a5
M. Hille <marciausgoslar@gmail.com>
2019-09-12 19:19:34 +0200

    Changed Birthday to Birth in userSchema

--- models.js
+++ models.js
@@ -15,7 +15,7 @@ const userSchema = mongoose.Schema({
     Username: {type: String, required: true},
     Password: {type: String, required: true},
     Email: {type: String, required: true},
-    Birthday: Date,
+    Birth: Date,
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: "Movie"}]
 });
 
