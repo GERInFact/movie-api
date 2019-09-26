0091282f810d81ff323af102b105ec345b5e8066
M. Hille <marciausgoslar@gmail.com>
2019-09-26 12:58:29 +0200

    Fixed bcrypt comapre error

--- models.js
+++ models.js
@@ -1,30 +1,34 @@
-const mongoose = require("mongoose"), bcrypt = require("bcrypt");
+const mongoose = require("mongoose"),
+  bcrypt = require("bcrypt");
 
 const saltRounds = 10;
 
 // title, description, genre, director, actors, imgUrl, featured
 const movieSchema = mongoose.Schema({
-    Title: {type: String, required: true},
-    Description: {type: String, required: true},
-    Genre: {Name: String, Description: String},
-    Director: {Name: String, Bio: String},
-    Actors: [String],
-    ImageUrl: String,
-    Featured: Boolean
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: { Name: String, Description: String },
+  Director: { Name: String, Bio: String },
+  Actors: [String],
+  ImageUrl: String,
+  Featured: Boolean
 });
 
 const userSchema = mongoose.Schema({
-    Username: {type: String, required: true},
-    Password: {type: String, required: true},
-    Email: {type: String, required: true},
-    Birth: Date,
-    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: "Movie"}]
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birth: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
 });
 
-userSchema.statics.hashPassword = (password) => bcrypt.hashSync(password, saltRounds);
-userSchema.methods.validatePassword = (password) => bcrypt.compareSync(password, this.Password);
+userSchema.statics.hashPassword = password =>
+  bcrypt.hashSync(password, saltRounds);
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
-const movieModel = mongoose.model('Movie', movieSchema);
+const movieModel = mongoose.model("Movie", movieSchema);
 const userModel = mongoose.model("User", userSchema);
 
 module.exports.Movie = movieModel;
