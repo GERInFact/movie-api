65bf8aaca7b6be8281ff7772d76da7df6c60e22b
M. Hille <marciausgoslar@gmail.com>
2019-11-08 09:51:14 +0100

    Added VisibilityFilter to movies-list view

--- client/src/components/movies-list/movies-list.jsx
+++ client/src/components/movies-list/movies-list.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import VisibilityFilterInput from "../visibility-filter-input/visibility-filter-input";
 import { MovieCard } from "../movie-card/movie-card";
 
 const mapStateToProps = state => {
@@ -17,7 +18,10 @@ function MoviesList(props) {
   }
   if (!movies) return <div className="main-view"></div>;
 
-  return filteredMovies.map(m => <MovieCard key={m._id} movie={m} />);
+  return (<div className="movies-list">
+      <VisibilityFilterInput visibilityFilter={visibilityFilter}/>
+      {filteredMovies.map(m => <MovieCard key={m._id} movie={m}/>)}
+  </div>)
 }
 
 export default connect(mapStateToProps)(MoviesList);
