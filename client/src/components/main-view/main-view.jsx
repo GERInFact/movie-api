45b7e91933c950926758da120a6051215fc34e91
M. Hille <marciausgoslar@gmail.com>
2019-11-08 12:01:10 +0100

    Rebuild react app

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -48,7 +48,7 @@ export class MainView extends React.Component {
       .get("https://my-flix-gerinfact.herokuapp.com/movies", {
         headers: { Authorization: `Bearer ${token}` }
       })
-      .then(res => {console.log(res.data)})
+      .then(res => {console.log(this.props)})
       .catch(err => {
         this.setState({ loadingMessage: "Connection Error: No movies found." });
         console.log(err.message);
