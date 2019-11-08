bf90e553c7f7febfb38a3ccff7ac1f2af6f20b78
M. Hille <marciausgoslar@gmail.com>
2019-11-08 09:09:45 +0100

    Implemented redux reducer logic

--- client/src/reducers/reducers.js
+++ client/src/reducers/reducers.js
@@ -1,4 +1,4 @@
-import { combinedReducers } from "redux";
+import { combineReducers } from "redux";
 import { SET_FILTER, SET_MOVIES } from "../actions/actions";
 
 function visibilityFilter(state = "", action) {
@@ -19,11 +19,9 @@ function movies(state = [], action) {
   }
 }
 
-function moviesApp(state = {}, action) {
-  return {
-    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
-    movies: movies(state.movies, action)
-  };
-}
+const moviesApp = combineReducers({
+  visibilityFilter,
+  movies
+});
 
 export default moviesApp;
