32321a5160e077284b9492e3c860e9e12ae1d472
M. Hille <marciausgoslar@gmail.com>
2019-11-08 09:13:30 +0100

    Added redux provider to index.jsx

--- client/src/reducers/reducers.js
+++ client/src/reducers/reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { SET_FILTER, SET_MOVIES } from "../actions/actions";
 
+
+
 function visibilityFilter(state = "", action) {
   switch (action.type) {
     case SET_FILTER:
